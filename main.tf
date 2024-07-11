4cca9f45c98505d1f236776535275b2bbb157d58
Gierado Pham <gieradopham25@Gierados-MBP.home>
2024-07-11 11:30:58 -0700

    update to main.tf file

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket-phamg"
 
   tags = {
-    Name        = "My bucket - bob"
+    Name        = "My bucket"
     Environment = "Dev"
   }
 }
