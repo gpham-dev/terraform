1afb7b0bb49c70ed94cd6ef442f4159cbc441406
Gierado Pham <gieradopham25@Gierados-MBP.home>
2024-07-11 13:46:28 -0700

    test4

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket-phamg"
 
   tags = {
-    Name        = "My bucket-test11"
+    Name        = "My bucket-test12"
     Environment = "Dev"
   }
 }
